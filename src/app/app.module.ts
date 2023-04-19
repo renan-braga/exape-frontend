e55ca2d423bcac07999f3d45d7a85d714b2f1864
renan <psico.bragarenan@gmail.com>
2023-04-19 17:42:03 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InteracaoComponent } from './components/interacao/interacao.component';
+import { ChamadoComponent } from './components/chamado/chamado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InteracaoComponent,
+    ChamadoComponent
   ],
   imports: [
     BrowserModule,
